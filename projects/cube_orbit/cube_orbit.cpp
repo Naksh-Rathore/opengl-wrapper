394109154c73e9aade82a3cf86a88ea6851ff9a9
Naksh-Rathore <naksh.rathore786@gmail.com>
2025-12-07 16:25:04 -0600

    Remove blank.png, add better placeholder textures, and add cube orbit project

---
+++ projects/cube_orbit/cube_orbit.cpp
@@ -0,0 +1,279 @@
+#include <iostream>
+#include <array>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h> 
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <stb_image.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 800
+
+const char *vertShaderSource {
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 2) in vec2 aTexCoord;\n"
+"\n"
+"out vec2 TexCoord;\n"
+"\n"
+"uniform mat4 model;\n"
+"uniform mat4 view;\n"
+"uniform mat4 projection;\n"
+"\n"
+"void main()\n"
+"{\n"
+"    gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
+"    TexCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
+"}\n"
+};
+
+const char *fragShaderSource {
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"\n"
+"in vec2 TexCoord;\n"
+"\n"
+"// texture samplers\n"
+"uniform sampler2D texture1;\n"
+"uniform sampler2D texture2;\n"
+"\n"
+"void main()\n"
+"{\n"
+"    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.4);\n"
+"}\n"
+};
+
+void processInput(GLFWwindow*window);
+void framebufferSizeCallback(GLFWwindow *window, int width, int height);
+
+int main() {
+    
+    if (!glfwInit()) {
+        std::cerr << "GLFW failed to initialize!\n";
+        return EXIT_FAILURE;
+    }
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window { glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGL", nullptr, nullptr) };
+
+    if (window == nullptr) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD\n";
+        
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+
+    GLuint VBO, VAO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    GLuint texture1, texture2;
+
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+
+    unsigned char *data { stbi_load("assets/container.jpg", &width, &height, &nrChannels, 0) };
+    
+    if (data == nullptr) {
+        std::cerr << "Couldn't load texture 1" << "\n";
+        return EXIT_FAILURE;
+    }
+    
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(data);
+
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    data = stbi_load("assets/awesomeface.png", &width, &height, &nrChannels, 0);
+    
+    if (data == nullptr) {
+        std::cerr << "Couldn't load texture 2" << "\n";
+        return EXIT_FAILURE;
+    }
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(data);
+
+    GLuint vert { glCreateShader(GL_VERTEX_SHADER) };
+    GLuint frag { glCreateShader(GL_FRAGMENT_SHADER) };
+
+    glShaderSource(vert, 1, &vertShaderSource, nullptr);
+    glCompileShader(vert);
+
+    glShaderSource(frag, 1, &fragShaderSource, nullptr);
+    glCompileShader(frag);
+
+    GLuint shaderProg { glCreateProgram() };
+
+    glAttachShader(shaderProg, vert);
+    glAttachShader(shaderProg, frag);
+
+    glLinkProgram(shaderProg);
+
+    glDeleteShader(vert);
+    glDeleteShader(frag);
+
+    glUseProgram(shaderProg);
+
+    glUniform1i(glGetUniformLocation(shaderProg, "texture1"), 0);
+    glUniform1i(glGetUniformLocation(shaderProg, "texture2"), 1);
+
+    glEnable(GL_DEPTH_TEST);
+
+    std::array<glm::vec3, 2> cubePositions = {
+        glm::vec3( 0.0f,  0.0f,  0.0f), 
+        glm::vec3( 0.0f,  1.0f, -3.0f)
+    }; 
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        processInput(window);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        
+    glUseProgram(shaderProg);
+
+        glm::mat4 view { glm::mat4(1.0f) };
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -10.0f)); 
+
+        glm::mat4 projection { glm::perspective(glm::radians(45.0f), 800.0f / 800.0f, 0.1f, 100.0f) };
+
+        glUniformMatrix4fv(glGetUniformLocation(shaderProg, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProg, "view"), 1, GL_FALSE, glm::value_ptr(view));
+
+        for (int i = 0; i < cubePositions.size(); i++) {
+            glm::mat4 model { glm::mat4(1.0f) };
+
+            // Second cube
+            if (i == 1)
+                model = glm::rotate(model, glm::radians( (float) glfwGetTime() * i * 50), glm::vec3(0.5f, 1.0f, 0.0f)); 
+
+            // First cube
+            else
+                model = glm::rotate(model, (float) glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f)); 
+                        
+            model = glm::translate(model, cubePositions.at(i));
+
+            glUniformMatrix4fv(glGetUniformLocation(shaderProg, "model"), 1, GL_FALSE, glm::value_ptr(model));
+                
+            glBindVertexArray(VAO);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebufferSizeCallback(GLFWwindow *window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
