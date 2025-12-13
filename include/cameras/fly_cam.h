42a8f45dff7d7b825199b6577ed3d26d4a977246
Naksh-Rathore <naksh.rathore786@gmail.com>
2025-12-12 22:15:22 -0600

    Add fly camera class

---
+++ include/cameras/fly_cam.h
@@ -0,0 +1,76 @@
+#ifndef FLY_H
+#define FLY_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+enum CameraDirection {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+};
+
+class FlyCamera {
+    private:
+        glm::vec3 m_pos{};
+        glm::vec3 m_front{};
+        glm::vec3 m_up{};
+        glm::vec3 m_direction{};
+        glm::vec3 m_right{};
+        glm::vec3 m_worldUp{};
+
+        float m_yaw{};
+        float m_pitch{};
+
+        float m_camSpeed{};
+        float m_mouseSensi{};
+        float m_zoom{};
+
+        void updateCameraVectors();
+
+    public:
+        FlyCamera(const glm::vec3& pos, const glm::vec3& front, const glm::vec3& up, float yaw, float pitch, float camSpeed, float mouseSensi);
+
+        glm::mat4 viewMatrix();
+
+        void processKeyboardInput(CameraDirection direction, float deltaTime);
+        void processMouseInput(float xOffset, float yOffset, bool stopRoll = true);
+        void processScrollInput(float yOffset);
+
+        const glm::vec3& pos() const { return m_pos; }
+        void pos(const glm::vec3& val) { m_pos = val; }
+
+        const glm::vec3& front() const { return m_front; }
+        void front(const glm::vec3& val) { m_front = val; }
+
+        const glm::vec3& up() const { return m_up; }
+        void up(const glm::vec3& val) { m_up = val; }
+
+        const glm::vec3& direction() const { return m_direction; }
+        void direction(const glm::vec3& val) { m_direction = val; }
+
+        const glm::vec3& right() const { return m_right; }
+        void right(const glm::vec3& val) { m_right = val; }
+
+        const glm::vec3& worldUp() const { return m_worldUp; }
+        void worldUp(const glm::vec3& val) { m_worldUp = val; }
+
+        float yaw() const { return m_yaw; }
+        void yaw(float val) { m_yaw = val; }
+
+        float pitch() const { return m_pitch; }
+        void pitch(float val) { m_pitch = val; }
+
+        float camSpeed() const { return m_camSpeed; }
+        void camSpeed(float val) { m_camSpeed = val; }
+
+        float mouseSensi() const { return m_mouseSensi; }
+        void mouseSensi(float val) { m_mouseSensi = val; }
+
+        float zoom() const { return m_zoom; }
+        void zoom(float val) { m_zoom = val; }      
+};
+    
+#endif
