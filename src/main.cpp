907a20de9145c2e8a11386cb7c827f74dffecbf9
Naksh-Rathore <naksh.rathore786@gmail.com>
2025-12-25 18:44:13 -0600

    Make default screen color opaque (1.0f)

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main() {
     glEnable(GL_DEPTH_TEST);
 
     while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         processInput(window);
